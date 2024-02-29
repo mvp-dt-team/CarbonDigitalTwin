c2ee10056bd6471f33cc76f801606230800dd37a
MKondakova <milanon@ya.ru>
2024-02-29 20:50:55 +0300

    made updates in db structure

--- modbus_to_sql/fill_db.sql
+++ modbus_to_sql/fill_db.sql
@@ -1,20 +1,30 @@
+# CREATE DATABASE digital_twin_database;
 USE digital_twin_database;
 
+# Информация о моделях сенсоров
 INSERT INTO sensor (name, description)
 VALUES ('Терморегулятор ESM', 'Поддерживает подключение термопары и нагревателя\\охладителя'),
-       ('Реле а345', 'Поддерживает подключение по modbus');
+       ('Реле а345', 'Поддерживает подключение по modbus'),
+       ('Другой датчик для примера', 'просто пример');
 
-INSERT INTO sensor_item (sensor_id, is_active)
-VALUES (1, TRUE),
-       (2, FALSE);
+# Информация о конкретных установленных датчиках
+INSERT INTO sensor_item (sensor_id, is_active, sensor_type)
+VALUES (1, TRUE, 'modbus'),
+       (2, FALSE, 'random'),
+       (3, TRUE, 'random');
 
+# Информация о месте получения данных
 INSERT INTO measurement_source (name, units, description)
 VALUES ('Температура у входа в фильеру', 'Цельсии', NULL),
        ('Состояние нагревателя у входа в фильеру', 'Включен/выключен', 'Управляется за счет температуры');
 
-INSERT INTO connection_params (sensor_item_id, param_name, param_value)
-VALUES (1, 'type', 'modbus'),
-       (1, 'address', '1');
+# Информация о свойствах конкретного датчика, какой адрес у датчика, по каким регистрам опрашивать данные
+INSERT INTO sensor_params (sensor_item_id, property_id, param_name, param_value)
+VALUES (1, null, 'address', '1'),
+       (1, 1, 'location', 'HOLDING_REGISTERS'),
+       (1, 1, 'register', '1');
 
+# Информация о том, какие конкретные данные получает один конкретный датчик
 INSERT INTO sensor_source_mapping
-VALUES (1, 1)
+VALUES (1, 1),
+       (2, 3);
--- modbus_to_sql/refresh_db.sql
+++ modbus_to_sql/refresh_db.sql
@@ -3,6 +3,7 @@ DROP DATABASE IF EXISTS digital_twin_database;
 CREATE DATABASE IF NOT EXISTS digital_twin_database;
 USE digital_twin_database;
 
+# Информация о моделях сенсоров
 CREATE TABLE IF NOT EXISTS sensor
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
@@ -10,6 +11,7 @@ CREATE TABLE IF NOT EXISTS sensor
     description TEXT
 );
 
+# Информация о месте получения данных
 CREATE TABLE IF NOT EXISTS measurement_source
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,16 +20,19 @@ CREATE TABLE IF NOT EXISTS measurement_source
     description TEXT
 );
 
+# Информация о конкретных установленных датчиках
 CREATE TABLE IF NOT EXISTS sensor_item
 (
     id                INT PRIMARY KEY AUTO_INCREMENT,
     sensor_id         INT                                NOT NULL,
     installation_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     is_active         BOOLEAN                            NOT NULL,
+    sensor_type       VARCHAR(50) NOT NULL,
     addition_info     text,
     FOREIGN KEY (sensor_id) REFERENCES sensor (id)
 );
 
+# Информация о том, какие конкретные данные получает один конкретный датчик
 CREATE TABLE IF NOT EXISTS sensor_source_mapping
 (
     measurement_source_id INT NOT NULL,
@@ -37,6 +42,7 @@ CREATE TABLE IF NOT EXISTS sensor_source_mapping
     FOREIGN KEY (sensor_item_id) REFERENCES sensor_item (id)
 );
 
+# Информация о свойствах конкретного датчика, какой адрес у датчика, по каким регистрам опрашивать данные
 CREATE TABLE IF NOT EXISTS sensor_params
 (
     id             INT PRIMARY KEY AUTO_INCREMENT,
@@ -48,7 +54,7 @@ CREATE TABLE IF NOT EXISTS sensor_params
     FOREIGN KEY (property_id) REFERENCES measurement_source (id)
 );
 
-
+# Информация о измерениях, снятых с датчиков
 CREATE TABLE IF NOT EXISTS measurement
 (
     query_id              INT   NOT NULL,
@@ -61,6 +67,7 @@ CREATE TABLE IF NOT EXISTS measurement
     FOREIGN KEY (measurement_source_id) REFERENCES measurement_source (id)
 );
 
+# Возможные другие данные
 CREATE TABLE IF NOT EXISTS raw_data
 (
     query_id  INT  NOT NULL,
