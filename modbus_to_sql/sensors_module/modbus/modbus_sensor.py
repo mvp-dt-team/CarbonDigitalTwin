440a75b056d5117f431d6b31dff974b678ba416e
MKondakova <milanon@ya.ru>
2024-03-04 00:56:00 +0300

    add random type sensor

--- modbus_to_sql/sensors_module/modbus/modbus_sensor.py
+++ modbus_to_sql/sensors_module/modbus/modbus_sensor.py
@@ -1,7 +1,8 @@
-from typing import Any, Sequence
-from modbus_to_sql.sensors_module.modbus.com_client import COMClient
+from typing import Any, Sequence, Dict
+from modbus_to_sql.sensors_module.modbus.modbus_rtu_client import ModbusRTUClient
 from modbus_to_sql.sensors_module.sensor import Sensor
-from modbus_to_sql.sensors_module.modbus.modbus_property import ModbusProperty, RegisterLocation, modbus_data_type_from_str
+from modbus_to_sql.sensors_module.modbus.modbus_property import ModbusProperty, RegisterLocation, \
+    modbus_data_type_from_str
 from pymodbus.exceptions import ModbusIOException
 
 from modbus_to_sql.sensors_module.unit import get_unit_from_str
@@ -9,19 +10,23 @@ from modbus_to_sql.network_models.active_sensors_response import ActiveSensorsRe
 
 
 class ModbusSensor(Sensor):
-    def __init__(self, title: str, id: int,
-                 properties: Sequence[ModbusProperty],
+    def readAllProperties(self) -> Dict[int, Any]:
+        return {p_id: self.readPropertyData(p_id) for p_id in self.properties}
+
+    def __init__(self, title: str, s_id: int,
+                 properties: Dict[int, ModbusProperty],
                  address: int) -> None:
-        super().__init__(title, id, properties)
+        super().__init__(title, s_id, properties)
         self.connection = None
         self.address = address
 
-    def init_from_network(self, sensor_data: ActiveSensorsResponseItem) -> None:
+    @staticmethod
+    def init_from_network(sensor_data: ActiveSensorsResponseItem) -> ModbusSensor:
         # добавить проверку наличия свойств и выдать соответствующие ошибки
         address = int(sensor_data.parameters.get("address", 0))
 
-        properties = [
-            ModbusProperty(
+        properties = {
+            prop.p_id: ModbusProperty(
                 id=prop.p_id,
                 name=prop.name,
                 unit=get_unit_from_str(prop.unit),
@@ -30,34 +35,32 @@ class ModbusSensor(Sensor):
                 dataType=modbus_data_type_from_str(prop.parameters.get("data_type", ""))
             )
             for prop in sensor_data.properties
-        ]
+        }
 
-        super().__init__(title=sensor_data.s_type, id=sensor_data.s_id, properties=properties)
-        self.connection = None
-        self.address = address
+        return ModbusSensor(sensor_data.s_type, sensor_data.s_id, properties, address)
 
-    def set_connection(self, connection: COMClient):
+    def set_connection(self, connection: ModbusRTUClient):
         self.connection = connection
 
     def __str__(self):
         return (self.title + '(m_addr: ' + str(self.address) + ')' if self.address is not None else self.title) + str(
             self.properties)
 
-    def readPropertyData(self, property_index: int) -> Any:
+    def readPropertyData(self, property_id: int) -> Any:
         if self.connection.is_connected() and self.connection.modbus_client is not None:
             try:
                 client = self.connection.modbus_client
-                property = self.properties[property_index]
-                if not isinstance(property, ModbusProperty):
+                prop = self.properties[property_id]
+                if not isinstance(prop, ModbusProperty):
                     raise TypeError("Ожидается поле типа ModbusProperty")
                 modbus_response = None
-                if property.location == RegisterLocation.HOLDING_REGISTERS:
+                if prop.location == RegisterLocation.HOLDING_REGISTERS:
                     modbus_response = client.read_holding_registers(
-                        property.address, slave=self.address)
+                        prop.address, slave=self.address)
 
                 if modbus_response is None:
                     raise Exception(
-                        f"Данная позиция регистра {str(property.location)} не поддерживается")
+                        f"Данная позиция регистра {str(prop.location)} не поддерживается")
 
                 if modbus_response.isError():
                     print("Error reading register!")
