a6cce8c3b2bbf4ead4b8e83e5afaa9a1d91985da
MKondakova <milanon@ya.ru>
2024-03-11 19:03:27 +0300

    add sensors module demonstration

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 from sensors_module.sensors_module import SensorsModule
+from sensors_module.ui import SensorModuleUI
 
 m = SensorsModule()
+app = SensorModuleUI(lambda: m.start(app.update_interface), m.stop)
+
+app.mainloop()
