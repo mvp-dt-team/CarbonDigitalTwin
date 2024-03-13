7844d0321011233fdd590332fd7eec3f58a2de03
MKondakova <milanon@ya.ru>
2024-03-13 17:10:46 +0300

    add second schema

--- aspen-project/main.py
+++ aspen-project/main.py
@@ -1,55 +1,45 @@
 from CodeLibraryCustom import Simulation
+import os
 
-sim = Simulation(AspenFileName= "Styrene.bkp", WorkingDirectoryPath= r"C:\\Users\\boiko.k.v\\Desktop\\pythonprojects\\schemes" ,VISIBILITY=False)
+working_path = os.getcwd()
+
+## на вход принимаются разные файлы для двух кейсов
+sim = Simulation(AspenFileName="StyreneWithoutOptimizer.bkp", WorkingDirectoryPath=working_path + "\\schemes",
+                 VISIBILITY=False)
 
 
-def setParam(amountEB, simulation):
-    # print(Simulation.__dir__())
+def calculation_column_params(amountEB, simulation):
     simulation.STRM_Set_TotalFlowBasis(Streamname='10-FEED', TotalFlowBasis=amountEB, Compoundname='ETHYLBEN')
-    convergence = sim.Run()
+    simulation.Run()
     col11_out = simulation.BLK_RADFRAC_GET_OUTPUTS('COL11')
     col21_out = simulation.BLK_RADFRAC_GET_OUTPUTS('COL21')
-    return col11_out['Condenser_RefluxRatio'], col11_out['Reboiler_BottomsToFeedRatio'], col21_out['Reboiler_BottomsToFeedRatio'],
+    return col11_out['Condenser_RefluxRatio'], col11_out['Reboiler_BottomsToFeedRatio'], col21_out[
+        'Reboiler_BottomsToFeedRatio'],
 
-import tkinter as tk
 
-def process_number():
-    try:
-        # Получаем число из текстового поля
-        input_number = float(entry.get())
-        # Пример обработки: удвоение и утроение введенного числа
-        output_number1, output_number2, output_number3 = setParam(input_number, sim)
-        # Обновляем метки результатов
-        result_label1.config(text=f"Флегмовое число на колонне 1:: {output_number1}")
-        result_label2.config(text=f"Отношение кубового потока к сырьевому потоку на колонне 1: {output_number2}")
-        result_label3.config(text=f"Отношение кубового потока к сырьевому потоку на колонне 2: {output_number3}")
-    except ValueError:
-        # Обработка случая, когда введено не число
-        result_label1.config(text="Ошибка: введите число")
-        result_label2.config(text="")
+calculation_column_params(50, sim)
+sim.CloseAspen()
 
-# Создаем главное окно
-root = tk.Tk()
-root.minsize(300, 200)
-root.title("Aspen UI")
+sim = Simulation(AspenFileName="Styrene.bkp", WorkingDirectoryPath=working_path + "\\schemes", VISIBILITY=False)
 
-# Создаем виджеты
-entry_label = tk.Label(root, text="Мольный поток C6H5C2H5:")
-entry = tk.Entry(root)
-process_button = tk.Button(root, text="Обработать", command=process_number)
-result_label1 = tk.Label(root, text="Флегмовое число на колонне 1: ")
-result_label2 = tk.Label(root, text="Отношение кубового потока к сырьевому потоку на колонне 1: ")
-result_label3 = tk.Label(root, text="Отношение кубового потока к сырьевому потоку на колонне 2: ")
 
-# Располагаем виджеты
-entry_label.pack()
-entry.pack()
-process_button.pack()
-result_label1.pack()
-result_label2.pack()
-result_label3.pack()
+def calculation_flow_composition(reflux_ratio: float, bottoms_to_feed_ratio: float, simulation):
+    cases = {
+        "60-VAPOR": ["ETHYLBEN", "TOLUENE", "BENZENE", "STYRENE", "ETHYLENE", "HYDROGEN", "METHANE", "WATER"],
+        "56-ORGAN": ["ETHYLBEN", "TOLUENE", "BENZENE", "STYRENE", "ETHYLENE", "HYDROGEN", "METHANE", "WATER"],
+        "64-STYRE": ["ETHYLBEN", "TOLUENE", "BENZENE", "STYRENE", "ETHYLENE", "HYDROGEN", "METHANE", "WATER"],
+        "62-ORGAN": ["ETHYLBEN", "TOLUENE", "BENZENE", "STYRENE", "ETHYLENE", "HYDROGEN", "METHANE", "WATER"]
+    }
+    simulation.BLK_RADFRAC_Set_Refluxratio('COL11', reflux_ratio)
+    simulation.BLK_RADFRAC_Set_BottomToFeedRatio('COL11', bottoms_to_feed_ratio)
+    simulation.Run()
+    result = {}
+    for stream in cases:
+        result[stream]['Общее'] = simulation.STRM_Get_MassFlowMixed(stream)
+        result[stream] = {}
+        for compound in cases[stream]:
+            result[stream][compound] = simulation.STRM_Get_MassFracPerCompound(stream, compound)
 
-# Запускаем главный цикл обработки событий
-root.mainloop()
 
+calculation_flow_composition(50, 0.6, sim)
 sim.CloseAspen()
