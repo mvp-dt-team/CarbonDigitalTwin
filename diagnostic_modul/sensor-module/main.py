245bdcbcb028b74b1747c3357df85bb33f7f44be
Бойко Константин Витальевич <bokovik2016@ya.ru>
2024-03-28 15:31:06 +0300

    fill module Handler Source Model

--- diagnostic_modul/sensor-module/main.py
+++ diagnostic_modul/sensor-module/main.py
@@ -1,7 +1,17 @@
-from classes import Source, Handler, Model
+from classes import Handler, Source, Model
+import os
 
-handler = Handler()
+sources = [
+    Source(id=0, unit=1, number=1, address=os.path.abspath('C:\\Users\\boiko.k.v\\Desktop\\Carbon-Digital-Twin\\diagnostic_modul\\sensor-module\\data\\videos\\test-video-1.mp4'))
+]
 
-models  = []
-sources = []
+print(os.path.abspath('./data/videos/test-video-1.mp4'))
+
+models = [
+    Model(version='0.0.1', path='C:\\Users\\boiko.k.v\\Desktop\\Carbon-Digital-Twin\\diagnostic_modul\\sensor-module\\data\\models\\best.pt')
+]
+
+test_handler = Handler()
+test_handler.add_source_model_mapping(source=sources[0], model=models[0])
+test_handler.run()
 
