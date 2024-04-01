f323efd76e83ab2bf1c36013d245f97751ba3fdb
MKondakova <milanon@ya.ru>
2024-04-01 10:26:20 +0300

    add admin ui

--- network_models/measurements_info.py
+++ network_models/measurements_info.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
@@ -8,6 +8,7 @@ class Measurement(BaseModel):
     m_data: float
     sensor_item_id: int
     measurement_source_id: int
+    insert_ts: Optional[datetime] = None
 
 
 class MeasurementsInfo(BaseModel):
