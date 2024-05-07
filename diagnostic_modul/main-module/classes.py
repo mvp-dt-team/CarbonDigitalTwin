0081f659baf132af4aec0d5b793444bcc2c15bae
Бойко Константин Витальевич <bokovik2016@ya.ru>
2024-05-07 21:31:33 +0300

    complete module property prediction and test server

--- diagnostic_modul/main-module/classes.py
+++ diagnostic_modul/main-module/classes.py
@@ -5,124 +5,142 @@ from enum import Enum
 from datetime import datetime
 import time
 import sys
+import requests
+import os
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+import json
 
-class TYPE_DATA(Enum):
-    VALIDATION = 0
-    TRAIN = 1
+import warnings
+from sklearn.exceptions import DataConversionWarning
+warnings.filterwarnings(action='ignore', category=UserWarning)
 
-class STATUS(Enum):
-    TRAIN = 0
-    PREDICT = 1
-
-# Заглушка
 class Sensor:
-    def __init__(self, id: int, type_sensor: str):
+    def __init__(self, id: int, measurement_source_id: int, type_sensor: str, name: str, unit: int, description: str, model_name: str, installation_time: str, deactivation_time: str):
         self.type_sensor = type_sensor
         self.id = id
+        self.measurement_source_id = measurement_source_id
+        self.name = name
+        self.unit = unit
+        self.description = description
+        self.model_name = model_name
+        self.installation_time = installation_time
+        self.deactivation_time = deactivation_time
 
-    def get_data(self):
-        if self.type_sensor == 'VIDEO':
-            return random.random()
-        return random.random()
 
 class Model:
-    def __init__(self, model_path: str, params: dict, version: str):
+    def __init__(self, model_path: str, params: dict, version: str, property_names: List[str]) -> None:
         self.model_path = model_path
         self.params = params
         self.version = version
-        self.status = 1  # Default to prediction status
-        self.validation_dataset = []
-        self.train_dataset = []
+        self.property_names = property_names
 
-    def clear_datasets(self) -> None:
-        self.validation_dataset = []
-        self.train_dataset = []
+    def predict(self, X) -> Dict[str, float]:
+        pass
+ 
 
-    def append_dataset(self, data: any, type_data: int) -> int:
-        if data is None:
-            return 1
-        if type_data == TYPE_DATA.TRAIN.value:
-            self.train_dataset.append(data)
-        elif type_data == TYPE_DATA.VALIDATION.value:
-            self.validation_dataset.append(data)
-        else:
-            return 1
-        return 0
-
-    def proccessing(self, data: any) -> Union[dict, int]:
-        if data is not None:
-            if self.status == STATUS.TRAIN.value:
-                pass
-            elif self.status == STATUS.PREDICT.value:
-                pass
-        else:
-            return 1
-
-    def train(self):
-        pass  # Placeholder for training the model
-
-    def validate(self):
-        pass  # Placeholder for model validation
-
-    def predict(self) -> Dict[str, float]:
-
-        return {}  # Placeholder for model prediction
-
+class RandomForestModel(Model):
+    def __init__(self, model_path: str, params: Dict, version: str, property_names: List[str]) -> None:
+        super().__init__(model_path, params, version, property_names)
+        with open(self.model_path, 'rb') as f:
+            self.model = pickle.load(f)
+    
+    # Возможно, стоит добавить иттерацию по результатам прогноза на случай определения нескольких свойств TODO
+    def predict(self, X) -> Dict[str, float]:
+        result = self.model.predict(np.array(X).reshape(1 ,-1))
+        return {"property_names": self.property_names, "values": result.tolist()}
+            
 
 class Block:
-    def __init__(self, model: Model, sensors: List[Sensor]):
+    def __init__(self, model: Model, sensors: List[Sensor], logger: logging.Logger):
         self.model = model
         self.sensors = sensors
+        self.logger = logger
 
-    def poll_sensors(self) -> Dict[str, Dict[str, float]]:
-        data = {}
+    def poll_sensors(self, url) -> Union[List[float], int]:
+        measurements = []
         for sensor in self.sensors:
-            data[sensor.id] = sensor.get_data()
-        return data
+            measurements.append({
+                "sensor_id": sensor.id,
+                "measurement_source_id": sensor.measurement_source_id,
+                "time_from": "2001-03-26T00:00:00Z",
+                "time_to": datetime.utcnow().replace(microsecond=0).isoformat() + 'Z'
+            })
+        try:
+            response = requests.post(url=f'http://{url}/measurement_data', json={'measurements': measurements})
+            if response.ok:
+                return [x['data'][-1] for x in json.loads(response.content)] 
+            else:
+                raise Exception(f"{response.status_code} {response.reason}")
+        except Exception as e:
+            self.logger.error(f"Ошибка опроса датчиков: {e}")
+            return 1
+    
+    def get_list_sensors_id(self) -> list:
+        return [x.measurement_source_id for x in self.sensors]
 
 class Handler:
-    def __init__(self, db_path: str, polling_interval: float):
+    def __init__(self, polling_interval: float, url: str) -> None:
         self.status = 0
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger('MainModule')
+        self.logger.setLevel(logging.DEBUG)
         self.blocks = []
         self.polling_interval = polling_interval
-        self.db_path = db_path
         self.running = True
+        self.url = url
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+
+        self.logger.addHandler(console_handler)
+        self.logger.info('Инициализация обработчика')
 
     def add_block(self, model: Model, sensors: List[Sensor]) -> int:
-        block = Block(model, sensors)
+        block = Block(model, sensors, self.logger)
         self.blocks.append(block)
         return 0
 
-    def run_ml_module(self, block: Block, data: dict) -> Dict[str, float]:
-        return block.model.proccessing(data)
-
-    def write_db_data(self, data: Dict[str, float]) -> int:
+    def write_db_data(self, data: Dict[str, float], sensors: List[int]) -> int:
         try:
-            # Write data to the database
-            return 0
+            response = requests.post(url=f'http://{self.url}/save_prediction_data', json={"sources": sensors, "property_names": data['property_names'], "values": data['values']})
+            if response.ok:
+                return 0
+            else:
+                raise Exception(f"{response.status_code}")
         except Exception as e:
             self.logger.error(f"Ошибка при записи в БД: {e}")
             return 1
         
     def action(self):
-        while self.running:
-            for block in self.blocks:
-                data = block.poll_sensors()
-                status = self.run_ml_module(block, data)
-                if type(status) == dict:
-                    if status['type'] == 'predict':
-                        self.write_db_data(status)
-                    elif status['type'] == 'train':
-                        pass
-                    else:
-                        self.running = False
-                        raise Exception(f"Некорректный статус модели: {status['type']}")
-                else:
-                    self.running = False
-                    raise Exception("Ошибка при обработке данных моделью")
+        self.logger.info('Модуль начал работать')
+        try:
+            while self.running:
+                for block in self.blocks:
+                    self.logger.info('Опрос датчиков')
+                    data = block.poll_sensors(self.url)
 
-            time.sleep(self.polling_interval)
+                    if type(data) is int:
+                        self.running = False
+                        raise Exception("Не удалось опросить датчики")
+                    else:
+                        self.logger.info('Передача данных модели')
+                        predict_value = block.model.predict(data)
+                        self.logger.debug(f'X: {data}, y: {predict_value}')
+
+                        if type(predict_value) == dict:
+                                self.logger.info('Отправка данных на сервер')
+                                self.write_db_data(predict_value, sensors=block.get_list_sensors_id())
+                        else:
+                            self.running = False
+                            raise Exception("Ошибка при обработке данных моделью")
+
+                time.sleep(self.polling_interval)
+        except Exception as exc:
+            self.logger.error(f'{exc}')
             
             
 
--- diagnostic_modul/test_server.py
+++ diagnostic_modul/test_server.py
@@ -1,5 +1,8 @@
 from flask import request, Flask, jsonify
 import os
+import datetime
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -13,6 +16,8 @@ for idx, filename in enumerate(os.listdir('./cameras')):
 
 for camera in cameras:
     models[camera['id']] = model_path
+
+### Модуль компьютерного зрения
 
 @app.route('/camera')
 def camera():
@@ -53,7 +58,116 @@ def archivate(camera_id):
 
     return 'Image archived for camera {}'.format(camera_id), 200
     
+### Модуль предсказания свойств
 
-    
+measurement_sources = [
+  {
+    "id": 0,
+    "name": "universal 1",      
+    "unit": 1
+  },
+  {
+    "id": 1,
+    "name": "universal 2",
+    "unit": 2
+  }
+]
+units = [[
+    {
+    "description": "Pressure_unit1",
+    "installation_time": "2020-01-01T00:00:00Z",
+    "deactivation_time": "2021-01-01T00:00:00Z",
+    "model_name": "Модель A"
+  },
+  {
+    "description": "Temperature_unit1",
+    "installation_time": "2020-05-01T00:00:00Z",
+    "deactivation_time": None,
+    "model_name": "Модель A"
+  },
+  {
+    "description": "Humidity_unit1",
+    "installation_time": "2020-05-01T00:00:00Z",
+    "deactivation_time": None,
+    "model_name": "Модель A"
+  }
+],
+[
+  {
+    "description": "Temperature_unit2",
+    "installation_time": "2020-05-01T00:00:00Z",
+    "deactivation_time": None,
+    "model_name": "Модель B"
+  },
+  {
+    "description": "Pressure_unit2",
+    "installation_time": "2020-05-01T00:00:00Z",
+    "deactivation_time": None,
+    "model_name": "Модель B"
+  }
+]]
+
+@app.route('/measurement_source')
+def measurement_source():
+    return jsonify(measurement_sources)
+
+@app.route('/sensors_by_source')
+def get_sensor_data():
+    sensor_id = request.args.get('id')
+    return jsonify(units[int(sensor_id)])
+
+@app.route('/measurement_data', methods=['POST'])
+def process_measurement_data():
+    data = request.json
+
+    if data is None or 'measurements' not in data:
+        return jsonify({"error": "Invalid request format"}), 400
+
+    measurements = data['measurements']
+    response_data = []
+
+    for measurement in measurements:
+        sensor_id = int(measurement.get('sensor_id'))
+        measurement_source_id = int(measurement.get('measurement_source_id'))
+        time_from = datetime.datetime.strptime(measurement.get('time_from'), "%Y-%m-%dT%H:%M:%SZ")
+        time_to = datetime.datetime.strptime(measurement.get('time_to'), "%Y-%m-%dT%H:%M:%SZ")
+
+        data_for_measurement = get_data_source(sensor_id, measurement_source_id, time_from, time_to)
+        response_data.append(data_for_measurement)
+
+    return jsonify(response_data)
+
+def get_data_source(sensor_id, measurement_source_id,  time_from, time_to):
+    df = pd.read_csv('data.csv')
+    df['Time'] = pd.to_datetime(df['Time'])
+    filtered_df = df[(df['Time'] >= time_from) & (df['Time'] <= time_to)]
+
+    sensor_name = units[measurement_source_id][sensor_id]['description']
+
+    return {
+        "measurement_source_id": sensor_id,
+        "data": filtered_df[sensor_name].tolist()
+    }
+
+"""
+Передаваемое значение JSON
+{
+"sources": [0, 1, 2, 3, 4, 5],
+"property_names": ["elastic_modulus"],
+"values": [19.231]
+}
+
+"""
+@app.route('/save_prediction_data', methods=['POST'])
+def save_data_prediction():
+    data = request.json
+    sources = data['sources']
+    property_names = data['property_names']
+    values = data['values']
+
+    print(f'Получены следуещие данные: источники({sources}), названия свойств({property_names}), значения({values})')
+
+    return {'status': 'ok'}
+
 if __name__ == '__main__':
     app.run(debug=True)
